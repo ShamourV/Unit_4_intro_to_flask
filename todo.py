4c3191bdf291a7632d3eb5f8e1fb6a4a47960a18
Svassell2@brooklynsteamcenter.org <svassell2@brooklynsteamcenter.org>
2024-01-02 10:50:59 -0500

    GG ez no re

--- todo.py
+++ todo.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-todos = [
-    "make bread",
-    "make more bread"
-]
+todos = ["make bread", "make more bread"]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    new_todo=request.form["new_todo"]
-    todos.append(new_todo)
-    return render_template ("todo.html.jinja", todos = todos)
+    if request.method == 'POST':
+        new_todos = request.form["new_todos"]
+        todos.append(new_todos)
+    return render_template ("todo.html.jinja", new_todos = todos)
+
+@app.route('/delete_todo/<int: todo_index>', methods= ['POST'])
+def todo_delete(todo_index):
+    del todos[todos]
+    return redirect('/')
