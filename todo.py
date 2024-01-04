8843b80b44be34e6aa0ff221bb7295a29e576d7a
Svassell2@brooklynsteamcenter.org <svassell2@brooklynsteamcenter.org>
2024-01-04 10:52:43 -0500

    No re plz

--- todo.py
+++ todo.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template, request, redirect
+import pymysql 
+import pymysql.cursors
+from pprint import pprint as print
+
+
+conn = pymysql.connect(
+    database="world",
+    user="svassell2",
+    password="228426979",
+    host="10.100.33.60",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
 
 app = Flask(__name__)
 
-todos = ["make bread", "make more bread"]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         new_todos = request.form["new_todos"]
-        todos.append(new_todos)
     return render_template ("todo.html.jinja", new_todos = todos)
 
 @app.route('/delete_todo/<int: todo_index>', methods= ['POST'])
